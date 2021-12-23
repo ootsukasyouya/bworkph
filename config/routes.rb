060cbe176d2974a01a526e908624fc00ea4f2134
ootsukasyouya <rikkuhose@gmail.com>
2021-12-23 20:38:30 +0900

    コメント機能

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :users
   resources :maps
   resources :stores
+  resources :conversations do
+  resources :messages
+  end
   post 'guest_login', to: "guest_sessions#create"
   post 'admin_guest_login', to: "admin_guest_sessions#create"
   namespace :admin do
