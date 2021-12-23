aec66de3f27c449c0d2f67ebe9e54211613b3017
ootsukasyouya <rikkuhose@gmail.com>
2021-12-23 23:53:09 +0900

    user編集

--- app/controllers/guest_sessions_controller.rb
+++ app/controllers/guest_sessions_controller.rb
@@ -4,10 +4,7 @@ class GuestSessionsController < ApplicationController
     user = User.find_or_create_by(email: "guest@exapmle.com") do |user|
       user.password = SecureRandom.urlsafe_base64
       user.name = "ゲストユーザー"
-      user.age = 1
       user.status = "会社員"
-      user.addres = "netto"
-      user.phone_number = 00000000
       user.admin = false
     end
       session[:user_id] = user.id
