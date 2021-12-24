5d7973d6cb7b6f83b54ca460acb520c7de4a29a0
ootsukasyouya <rikkuhose@gmail.com>
2021-12-24 19:19:28 +0900

    runsackgem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-10.times do |n|
+20.times do |n|
     User.create!(
       email: "test#{n + 1}@test.com",
-      name: "テスト太郎#{n + 1}",
+      name: "テスト店舗#{n + 1}",
       status: "店舗",
       password: "123456"
     )
   end
+
+  # 10.times do |n|
+  #   User.create!(
+  #     email: "test#{n + 1}@test.com",
+  #     name: "test_user#{n + 1}",
+  #     password: '123456',
+  #   )
+  # end
