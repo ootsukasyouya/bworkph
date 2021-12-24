45797fb6a583a8233ed98458b8f0e093c1bae940
ootsukasyouya <rikkuhose@gmail.com>
2021-12-24 14:48:23 +0900

    調整

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_23_102507) do
+ActiveRecord::Schema.define(version: 2021_12_24_045510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "comments", force: :cascade do |t|
-    t.text "comment"
-    t.integer "comment_id"
-    t.integer "commenter_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
   create_table "conversations", force: :cascade do |t|
     t.integer "sender_id"
@@ -79,11 +71,8 @@ ActiveRecord::Schema.define(version: 2021_12_23_102507) do
     t.string "name", null: false
     t.string "email", null: false
     t.string "password_digest", null: false
-    t.string "addres", null: false
-    t.string "phone_number", null: false
     t.boolean "admin", default: false
     t.integer "status", default: 0
-    t.integer "age", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
