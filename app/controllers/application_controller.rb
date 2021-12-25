6523e5a3e8e4928a711d21ee94026d9e3f2a99ae
ootsukasyouya <rikkuhose@gmail.com>
2021-12-25 16:27:19 +0900

    検索

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,11 @@ class ApplicationController < ActionController::Base
   def login_required
     redirect_to new_session_path unless current_user
   end
+  # def store_master!
+  #   if current_user.status == "店舗"
+  #
+  #   else
+  #     redirect_to user_show(current_user.id)
+  #   end
+  # end
 end
