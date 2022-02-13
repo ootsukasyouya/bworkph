fde2be6c38e585ed446f49431b78e04af236d6fa
ootsukasyouya <rikkuhose@gmail.com>
2022-02-13 18:48:47 +0900

    kakuninn

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,8 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import 'bootstrap';
 import '../stylesheets/application';
+require("@fortawesome/fontawesome-free");
+import '@fortawesome/fontawesome-free/js/all';
 
 
 Rails.start()
